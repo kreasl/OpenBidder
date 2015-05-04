6773780437c7a89e4cc4a110b155bf676d304212
Vitali Tsimoshka <v.tsimoshka@gmail.com>
2015-05-04 20:33:31 +0300

    Add openrtb-validator plugin
        Add sbt dependency
        Add example response and request from https://github.com/openrtb/examples
        Add simple plugin test

--- build.sbt
+++ build.sbt
@@ -9,3 +9,5 @@ libraryDependencies ++= Seq("org.specs2" %% "specs2-core" % "3.5" % "test")
 resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
 
 scalacOptions in Test ++= Seq("-Yrangepos")
+
+libraryDependencies +=  "org.openrtb" % "openrtb-validator" % "2.3.1"
---
+++ src/test/scala/OpenrtbValidatorSpec.scala
@@ -0,0 +1,29 @@
+import org.specs2.mutable.Specification
+import org.openrtb.validator._
+import scala.io.Source
+
+class OpenrtbValidatorSpec extends Specification {
+  "openrtb-validator simple lib test" >> {
+    "example request validation test" >> {
+      val request = Source.fromURL(
+        getClass().getResource("/examples/request.json")
+      ).mkString
+      val validator = OpenRtbValidatorFactory.getValidator(
+        OpenRtbInputType.BID_REQUEST,
+        OpenRtbVersion.V2_3
+      )
+      validator.isValid(request) must beTrue
+    }
+    "example response validation test" >> {
+      val response = Source.fromURL(
+        getClass().getResource("/examples/response.json")
+      ).mkString
+      val validator = OpenRtbValidatorFactory.getValidator(
+        OpenRtbInputType.BID_RESPONSE,
+        OpenRtbVersion.V2_3
+      )
+      validator.isValid(response) must beTrue
+    }
+  }
+
+}
