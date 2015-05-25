3a5cb24ab4161161de3cbc8349f77c0ea64917a2
Dzmitry Yarmilin <eds0404@gmail.com>
2015-05-26 01:31:10 +0300

    deserialization /serialization of bid request/response in DemandSideServer; validation of request/response json

---
+++ src/main/scala/com/openbidder/biddingserver/package.scala
@@ -0,0 +1,8 @@
+package com.openbidder
+
+import org.openrtb.validator.OpenRtbVersion
+
+package object biddingserver {
+  val ValidatorOpenRtbVersion = OpenRtbVersion.V2_3
+  val JsonContentType = "application/json"
+}
