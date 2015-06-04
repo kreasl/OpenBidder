4678bcd30a6aeeaf1309b9335e75fdce3333221e
eugen.nekhai <Joint123>
2015-06-04 16:33:09 +0300

    Simple exchange server

--- src/test/scala/com/openbidder/exchange/services/AuctionServiceSpec.scala
+++ src/test/scala/com/openbidder/exchange/services/AuctionServiceSpec.scala
@@ -1,6 +1,5 @@
 package com.openbidder.exchange.services
 
-import com.openbidder.model.bidrequest.Imp
 import com.openbidder.model.bidresponse.{Bid, BidResponse, SeatBid}
 import com.openbidder.model.com.openbidder.exchange.BidderConnection
 import com.openbidder.model.com.openbidder.exchange.services.AuctionService
@@ -18,53 +17,40 @@ class AuctionServiceSpec extends Specification with Mockito {
 
   val service = new AuctionService(List(new BidderConnection()))
 
-  val imp = mock[Imp]
-  imp.id returns "impid"
+  val seatBid = mockSeatBit(mockBid(10.22.toFloat))
 
-  val bid1 = mock[Bid]
-  bid1.id returns "bidId1"
-  bid1.impid returns "imp_id"
-  bid1.price returns 10.22.toFloat
+  val seatBid2 = mockSeatBit(mockBid(1.1.toFloat))
 
-  val bid2 = mock[Bid]
-  bid2.id returns "bidId2"
-  bid2.impid returns "imp_id2"
-  bid2.price returns 1.1.toFloat
+  val seatBid3 = mockSeatBit(mockBid(25.1.toFloat))
 
-  val bid3 = mock[Bid]
-  bid3.id returns "bidId3"
-  bid3.impid returns "imp_id3"
-  bid3.price returns 25.1.toFloat
+  val response1 = mockResponse(List(seatBid))
 
-  val seatBid = mock[SeatBid]
-  seatBid.bid returns List(bid1)
-  seatBid.group returns 0
+  val response2 = mockResponse(List(seatBid2))
 
-  val seatBid2 = mock[SeatBid]
-  seatBid2.bid returns List(bid2)
-  seatBid2.group returns 0
-
-  val seatBid3 = mock[SeatBid]
-  seatBid3.bid returns List(bid3)
-  seatBid3.group returns 0
-
-  val response1 = mock[BidResponse]
-  response1.id returns "responseId"
-  response1.seatbid returns List(seatBid)
-  response1.cur returns "USD"
-
-  val response2 = mock[BidResponse]
-  response2.id returns "responseId2"
-  response2.seatbid returns List(seatBid2)
-  response2.cur returns "USD"
-
-  val response3 = mock[BidResponse]
-  response3.id returns "responseId3"
-  response3.seatbid returns List(seatBid3)
-  response3.cur returns "USD"
+  val response3 = mockResponse(List(seatBid3))
 
   def e1 = service.winner(List(response1, response2, response3)).price must be_==(10.22.toFloat)
 
   def e2 = service.winner(List(response2)).price must be_==(1.1.toFloat)
 
+
+  def mockResponse(seatbid: Seq[SeatBid]): BidResponse = {
+    val response = mock[BidResponse]
+    response.id returns "respId"
+    response.seatbid returns seatbid
+    response.cur returns "USD"
+  }
+
+  def mockSeatBit(bid: Bid): SeatBid = {
+    val seatBid = mock[SeatBid]
+    seatBid.bid returns List(bid)
+    seatBid.group returns 0
+  }
+
+  def mockBid(price: Float): Bid = {
+    val bid = mock[Bid]
+    bid.id returns "id1"
+    bid.impid returns "impid1"
+    bid.price returns price
+  }
 }
